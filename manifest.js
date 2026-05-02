bd995d5e876f3f7ba3c662191a418cde2f6e9f5d
jotapf2294 <jotapft22@gmail.com>
2026-05-02 16:54:55 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+  "name": "Códice do Jota",
+  "short_name": "Códice",
+  "description": "Grimório agrícola digital offline para gestão da horta viva",
+
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#0C0A09",
+  "theme_color": "#7C2D12",
+
+  "orientation": "portrait",
+
+  "icons": [
+    {
+      "src": "/icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
